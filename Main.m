4f948d3a45ce3e445ae327e2105d14138262377b
pjmirand <pjmirand@espol.edu.ec>
2022-05-11 15:37:15 -0500

    mod superficie

--- Main.m
+++ Main.m
@@ -1,4 +1,4 @@
-%% clc, clear, close all
+clc, clear, close all
 
 %% 
 run('nuevo_DataFile.m') ; 
@@ -7,4 +7,4 @@ run('nuevo_DataFile.m') ;
 run('rigid_terrain_params.m');
 
 %%
-sim('nuevo.slx');
+sim('nuevo');
