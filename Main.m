facb6453a4e0b088b715eab7c849466c3d64548c
pjmirand <pjmirand@espol.edu.ec>
2022-06-13 13:45:39 -0500

    ground

--- Main.m
+++ Main.m
@@ -1,10 +1,14 @@
 clc, clear, close all
 
 %% 
-run('nuevo_DataFile.m') ; 
+%run('nuevo_DataFile.m') ; 
 
 %% Rigid Terrain Params
 run('rigid_terrain_params.m');
+
+sphere_radius = 0.1;
+sphere_ptcld  = Point_Cloud_Data_Sphere(sphere_radius,200);
+
 
 %%
 sim('nuevo');
--- modeloManual/Data.m
+++ modeloManual/Data.m
@@ -1,10 +1,25 @@
-a=0.2
-b=0.15
-R=0.16/2
 
-m1= [1 -1 -(a+b); 1 1 (a+b); 1 1 -(a+b); 1 -1 (a+b)]
-m2= (1/R)*m1
+run('Point_Cloud_Data_Cylinder.m');
 
-m3= pinv(m2)
-x0= 20;
-y0=20;
+
+%% Datos de la nube de puntos
+radius = 0.08;
+height = 0.03;
+caps = true;
+numpts = 50 ;
+rueda_ptcld  = Point_Cloud_Data_Cylinder(radius,height,numpts,caps);
+
+%rueda_ptcld  = Point_Could_Data_Circle(radius,200);
+
+
+%% 
+a=0.2;
+b=0.15;
+R=0.16/2;
+
+m1= [1 -1 -(a+b); 1 1 (a+b); 1 1 -(a+b); 1 -1 (a+b)];
+m2= (1/R)*m1;
+
+m3= pinv(m2);
+x0= 0;
+y0= 0;
